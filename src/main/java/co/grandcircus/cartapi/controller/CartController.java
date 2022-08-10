610b85ce66fbb8ce29af031eedcc9212232752dc
sam <samantha.underwood@gmail.com>
2022-08-10 19:23:26 -0400

    Commit to add error handling response body

--- src/main/java/co/grandcircus/cartapi/controller/CartController.java
+++ src/main/java/co/grandcircus/cartapi/controller/CartController.java
@@ -4,16 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import co.grandcircus.cartapi.exceptions.ItemNotFoundException;
 import co.grandcircus.cartapi.models.CartItems;
@@ -25,6 +16,13 @@ public class CartController {
 	@Autowired
 	private ItemRepository itemRepo;
 	
+	@ResponseBody
+	@ExceptionHandler(ItemNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	String itemNotFoundHandler(ItemNotFoundException ex) {
+		return ex.getMessage();
+	}
+	 
 	@GetMapping("/reset")
 	public String reset() {
 		// Delete all
